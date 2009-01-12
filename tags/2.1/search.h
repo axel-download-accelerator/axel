c17c7a3ca602a416df948584e045a4ea89b9584d
appaji <appaji@51c8d62b-5453-48d1-8312-aa2f93c8d43b>
2009-01-12 06:31:54 +0000

    Tagging 2.1 maintenance point (unreleased)

---
+++ tags/2.1/search.h
@@ -0,0 +1,37 @@
+  /********************************************************************\
+  * Axel -- A lighter download accelerator for Linux and other Unices. *
+  *                                                                    *
+  * Copyright 2001 Wilmer van der Gaast                                *
+  \********************************************************************/
+
+/* filesearching.com searcher include file				*/
+
+/*
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License with
+  the Debian GNU/Linux distribution in file /usr/doc/copyright/GPL;
+  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
+  Suite 330, Boston, MA  02111-1307  USA
+*/
+
+typedef struct
+{
+	char url[MAX_STRING];
+	double speed_start_time;
+	int speed, size;
+	pthread_t speed_thread[1];
+	conf_t *conf;
+} search_t;
+
+int search_makelist( search_t *results, char *url );
+int search_getspeeds( search_t *results, int count );
+void search_sortlist( search_t *results, int count );
