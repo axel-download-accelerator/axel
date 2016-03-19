fdea7fef760214cae1e51cb6e7cc0ce7a98048bd
Joao Eriberto Mota Filho <eriberto@debian.org>
2016-03-19 20:46:45 -0300

    Modernized the headers in source files and updated the GPL text

--- src/http.c
+++ src/http.c
@@ -1,28 +1,25 @@
-  /********************************************************************\
-  * Axel -- A lighter download accelerator for Linux and other Unices. *
-  *                                                                    *
-  * Copyright 2001 Wilmer van der Gaast                                *
-  * Copyright 2015 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br> *                                *
-  \********************************************************************/
-
-/* HTTP control file                                                   */
-
 /*
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  Axel -- A lighter download accelerator for Linux and other Unices
+
+  Copyright 2001-2007 Wilmer van der Gaast
+  Copyright 2015      Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License with
-  the Debian GNU/Linux distribution in file /usr/doc/copyright/GPL;
-  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-  Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+/* HTTP control file */
 
 #include "axel.h"
 
--- src/search.c
+++ src/search.c
@@ -1,27 +1,24 @@
-  /********************************************************************\
-  * Axel -- A lighter download accelerator for Linux and other Unices. *
-  *                                                                    *
-  * Copyright 2001 Wilmer van der Gaast                                *
-  \********************************************************************/
-
-/* filesearching.com searcher						*/
-
 /*
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
+  Axel -- A lighter download accelerator for Linux and other Unices
+
+  Copyright 2001-2007 Wilmer van der Gaast
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
-  You should have received a copy of the GNU General Public License with
-  the Debian GNU/Linux distribution in file /usr/doc/copyright/GPL;
-  if not, write to the Free Software Foundation, Inc., 59 Temple Place,
-  Suite 330, Boston, MA  02111-1307  USA
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
+
+/* filesearching.com searcher */
 
 #include "axel.h"
 
