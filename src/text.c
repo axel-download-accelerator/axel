feb11aadc3d98e29f44e1b94541e2345cb082f6c
Joao Eriberto Mota Filho <eriberto@debian.org>
2016-05-03 12:23:59 -0300

    Added Giridhar and Philipp to be shown by -V option

--- src/text.c
+++ src/text.c
@@ -594,6 +594,8 @@ void print_version()
 {
 	printf( _("\nAxel version %s (%s)\n"), AXEL_VERSION_STRING, ARCH );
 	printf( "\nCopyright 2001-2007 Wilmer van der Gaast," );
+	printf( "\n          2007-2009 Giridhar Appaji Nag," );
+	printf( "\n          2008-2010 Philipp Hagemeister," );
 	printf( "\n          2015-2016 Joao Eriberto Mota Filho," );
 	printf( "\n               2016 Stephen Thirlwall," );
 	printf( _("\n                    and others." ));
