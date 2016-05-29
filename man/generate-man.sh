755d4aa77c92cbd2af0e40d969ebddc52b867ad4
Joao Eriberto Mota Filho <eriberto@debian.org>
2016-05-29 15:57:40 -0300

    Using generate-man.sh instead of genallman.sh

---
+++ man/generate-man.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Generate the manpage
+# Copyright 2016 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
+# This file is under BSD-3-Clause
+
+P_DATA="29 May 2016"
+P_NAME=axel
+P_VERSION=2.8
+P_MANLEVEL=1
+P_DESCRIPT="light command line download accelerator"
+
+TEST=$(txt2man -h 2> /dev/null)
+
+[ ! "$TEST" ] && { echo "ERROR: You need install txt2man program."; exit 1; }
+
+[ -e $P_NAME.txt ] || { echo "ERROR: $P_NAME.txt not found."; exit 1; }
+
+txt2man -d "$P_DATA" -t $P_NAME -r $P_NAME-$P_VERSION -s $P_MANLEVEL -v "$P_DESCRIPT" $P_NAME.txt > $P_NAME.$P_MANLEVEL
