9760fc595195dcc25a8f24ffb521d9732ef74f38
Joao Eriberto Mota Filho <eriberto@debian.org>
2016-05-30 21:12:04 -0300

    Set 2.10 for next version

--- man/generate-man.sh
+++ man/generate-man.sh
@@ -4,9 +4,9 @@
 # Copyright 2016 Joao Eriberto Mota Filho <eriberto@eriberto.pro.br>
 # This file is under BSD-3-Clause
 
-P_DATA="29 May 2016"
+P_DATA="30 May 2016"
 P_NAME=axel
-P_VERSION=2.8
+P_VERSION=2.10
 P_MANLEVEL=1
 P_DESCRIPT="light command line download accelerator"
 
