48c8fd03c8a57b7cccd0def853c0c7036cb86376
Harshavardhana <harsha@minio.io>
2017-01-13 14:06:05 -0800

    For servers running on port, axel should set port properly.
    Host header should have a port set properly in accordance
    with HTTP RFC2616.
    
    https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.23

--- src/conn.c
+++ src/conn.c
@@ -276,7 +276,7 @@ int conn_setup( conn_t *conn )
 		snprintf( s, MAX_STRING, "%s%s", conn->dir, conn->file );
 		conn->http->firstbyte = conn->currentbyte;
 		conn->http->lastbyte = conn->lastbyte;
-		http_get( conn->http, s );
+		http_get( conn->http, conn->port, s );
 		http_addheader( conn->http, "User-Agent: %s", conn->conf->user_agent );
 		for( i = 0; i < conn->conf->add_header_count; i++)
 			http_addheader( conn->http, "%s", conn->conf->add_header[i] );
