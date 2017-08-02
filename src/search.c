c75a1f3ff703300d138d2fea98eb205ec39e8784
Ismael Luceno <ismael@iodev.co.uk>
2017-08-02 20:12:30 -0300

    Reduce scope of var s2 in search_makelist

--- src/search.c
+++ src/search.c
@@ -81,7 +81,7 @@ int main( int argc, char *argv[] )
 int search_makelist( search_t *results, char *url )
 {
 	int i, size = 8192, j = 0;
-	char *s, *s1, *s2, *s3;
+	char *s, *s1, *s3;
 	conn_t conn[1];
 	double t;
 
@@ -151,7 +151,7 @@ int search_makelist( search_t *results, char *url )
 	for( i = 1; strncmp( s1, "</pre>", 6 ) && i < results->conf->search_amount && *s1; i ++ )
 	{
 		s3 = strchr( s1, '\n' ); *s3 = 0;
-		s2 = axel_strrstr( s1, "<a href=" ) + 8;
+		char *s2 = axel_strrstr( s1, "<a href=" ) + 8;
 		*s3 = '\n';
 		s3 = strchr( s2, ' ' ); *s3 = 0;
 		if( strcmp( results[0].url, s2 ) )
