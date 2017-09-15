43a725bc4c41f47ae7371779559393c86749b65d
Antonio Quartulli <a@unstable.cc>
2017-09-15 17:13:18 +0800

    Move comment inside if block
    the comment is related to what we are doing inside
    the if block, therefore move it there to make the
    explanation more clear.
    
    Signed-off-by: Antonio Quartulli <a@unstable.cc>

--- src/axel.c
+++ src/axel.c
@@ -461,10 +461,10 @@ void axel_do( axel_t *axel )
 
 	timeval->tv_sec = 0;
 	timeval->tv_usec = 100000;
-	/* A select() error probably means it was interrupted
-	   by a signal, or that something else's very wrong... */
 	if( select( hifd + 1, fds, NULL, NULL, timeval ) == -1 )
 	{
+		/* A select() error probably means it was interrupted
+		 * by a signal, or that something else's very wrong... */
 		axel->ready = -1;
 		return;
 	}
