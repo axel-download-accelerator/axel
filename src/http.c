7e0ec41d43accb180aed39b2638d97fd87a2851c
Shankar <shankar_m@protonmail.com>
2018-04-27 17:25:52 +0000

    Strip leading hyphens in extracted filename
    Leading hyphens in filenames have been causing undpredictable behaviour
    in POSIX systems when passed as first argument to command line programs.
    Therefore the filename extracted from Content-Disposition header is
    stripped of any leading hyphens.
    
    By convention, programs accept their options as their first argument,
    usually preceeded by a hyphen. The shell lists files alphabetically when
    expanding wildcard '*' and hyphen comes first in lexicographic caste
    system. Therefore filenames with leading hyphens appear first when '*'
    is used and consequently considered as options to the invoked program,
    yielding unpredictable and dangerous behaviour.
    
    Signed-off-by: Shankar <shankar_m@protonmail.com>
    [ismael@iodev.co.uk: replaced code by mod to the sscanf fmt string]
    Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

--- src/http.c
+++ src/http.c
@@ -311,7 +311,7 @@ http_filename(const http_t *conn, char *filename)
 {
 	const char *h;
 	if ((h = http_header(conn, "Content-Disposition:")) != NULL) {
-		sscanf(h, "%*s%*[ \t]filename%*[ \t=\"\']%254[^\n\"\' ]",
+		sscanf(h, "%*s%*[ \t]filename%*[ \t=\"\'-]%254[^\n\"\' ]",
 		       filename);
 
 		/* Replace common invalid characters in filename
