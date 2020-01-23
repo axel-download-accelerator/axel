e303c11253074074a16e0ac0b636ae2ae57c9107
David Polverari <david.polverari@gmail.com>
2020-01-23 18:09:25 -0500

    Revert "Minor changes"
    This reverts commit c2fc839a40e1ebe1282e481963ceb433db981f9f.

--- src/netrc.c
+++ src/netrc.c
@@ -181,12 +181,9 @@ netrc_parse(netrc_t *netrc, const char *host, char *user, size_t user_len, char 
 	};
 	enum { parser_len = sizeof(parser) / sizeof(*parser), };
 
-	if (NULL == netrc)
-		return 0;
-
 	tok = memtok(netrc->s_addr, netrc->sz, tok_delim, &save_buf);
 	while (tok.len) {
-		const struct parser *p = parser;
+		struct parser *p = parser;
 		while (p < parser + parser_len && strncmp(p->key, tok.data, tok.len))
 			p++;
 
@@ -202,7 +199,7 @@ netrc_parse(netrc_t *netrc, const char *host, char *user, size_t user_len, char 
 				tok = memtok(NULL, 0, tok_delim, &save_buf);
 				if (!strncmp(host, tok.data, tok.len))
 					matched = true;
-			} else {
+			} else if (tok.data[0] == 'd') {
 				matched = true;
 			}
 		} else {
@@ -211,10 +208,11 @@ netrc_parse(netrc_t *netrc, const char *host, char *user, size_t user_len, char 
 				// FIXME should we be aborting?
 				if (tok.len >= p->len) {
 					tok.len = p->len - 1;
+					p->dst[tok.len] = 0;
 				}
 
-				memcpy(p->dst, tok.data, tok.len + 1);
-				p->dst[tok.len] = 0;
+				/* need strlcpy here, as tok.data isn't NULL-terminated */
+				strlcpy(p->dst, tok.data, tok.len + 1);
 			}
 		}
 
