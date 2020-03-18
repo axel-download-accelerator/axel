4c69886f17f34c2f6914303fbeeb3b68b5a4890d
Ted Reed <reed@fb.com>
2020-03-18 15:04:56 -0400

    Add iSEC Partners hostname validation strategy
    This should support OpenSSL 1.1.0 and 0.9.8.

--- src/ssl.c
+++ src/ssl.c
@@ -4,6 +4,7 @@
   Copyright 2016      Sjjad Hashemian
   Copyright 2016-2017 Stephen Thirlwall
   Copyright 2017      Antonio Quartulli
+  Copyright 2012      iSEC Partners (SSL hostname validation)
   Copyright 2017-2019 Ismael Luceno
 
   This program is free software; you can redistribute it and/or
@@ -42,11 +43,18 @@
 #ifdef HAVE_SSL
 
 #include <openssl/err.h>
-#include <openssl/x509.h>
+#include <openssl/x509v3.h>
 
-#ifndef X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS
-#define X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS 4
-#endif
+typedef enum {
+	MatchFound,
+	MatchNotFound,
+	NoSANPresent,
+	MalformedCertificate,
+	Error
+} validate_result;
+
+validate_result
+validate_hostname(const char *hostname, const X509 *server_cert);
 
 static pthread_mutex_t ssl_lock;
 static bool ssl_inited = false;
@@ -73,25 +81,131 @@ ssl_startup(void)
 	pthread_mutex_unlock(&ssl_lock);
 }
 
+static validate_result
+matches_common_name(const char *hostname, const X509 *server_cert)
+{
+	int common_name_loc = -1;
+	X509_NAME_ENTRY *common_name_entry = NULL;
+	ASN1_STRING *common_name_asn1 = NULL;
+	char *common_name_str = NULL;
+
+	// Find the position of the CN field in the Subject field of the certificate
+	common_name_loc = X509_NAME_get_index_by_NID(X509_get_subject_name((X509 *) server_cert), NID_commonName, -1);
+	if (common_name_loc < 0) {
+		return Error;
+	}
+
+	// Extract the CN field
+	common_name_entry = X509_NAME_get_entry(X509_get_subject_name((X509 *) server_cert), common_name_loc);
+	if (common_name_entry == NULL) {
+		return Error;
+	}
+
+	// Convert the CN field to a C string
+	common_name_asn1 = X509_NAME_ENTRY_get_data(common_name_entry);
+	if (common_name_asn1 == NULL) {
+		return Error;
+	}
+	#if OPENSSL_VERSION_NUMBER < 0x10101000L
+		common_name_str = (char *) ASN1_STRING_data(common_name_asn1);
+	#else
+		common_name_str = (char *) ASN1_STRING_get0_data(common_name_asn1);
+	#endif
+
+	// Make sure there isn't an embedded NUL character in the CN
+	if ((size_t) ASN1_STRING_length(common_name_asn1) != strlen(common_name_str)) {
+		return MalformedCertificate;
+	}
+
+	// Compare expected hostname with the CN
+	if (strcasecmp(hostname, common_name_str) == 0) {
+		return MatchFound;
+	} else {
+		return MatchNotFound;
+	}
+}
+
+static validate_result
+matches_subject_alternative_name(const char *hostname, const X509 *server_cert)
+{
+	validate_result result = MatchNotFound;
+	int i;
+	int san_names_nb = -1;
+	STACK_OF(GENERAL_NAME) *san_names = NULL;
+
+	// Try to extract the names within the SAN extension from the certificate
+	san_names = X509_get_ext_d2i((X509 *) server_cert, NID_subject_alt_name, NULL, NULL);
+	if (san_names == NULL) {
+		return NoSANPresent;
+	}
+	san_names_nb = sk_GENERAL_NAME_num(san_names);
+
+	// Check each name within the extension
+	for (i = 0; i < san_names_nb; i++) {
+		const GENERAL_NAME *current_name = sk_GENERAL_NAME_value(san_names, i);
+
+		if (current_name->type == GEN_DNS) {
+			// Current name is a DNS name, let's check it
+#if OPENSSL_VERSION_NUMBER < 0x10101000L
+			char *dns_name = (char *) ASN1_STRING_data(current_name->d.dNSName);
+#else
+			char *dns_name = (char *) ASN1_STRING_get0_data(current_name->d.dNSName);
+#endif
+			// Make sure there isn't an embedded NUL character in the DNS name
+			if ((size_t) ASN1_STRING_length(current_name->d.dNSName) != strlen(dns_name)) {
+				result = MalformedCertificate;
+				break;
+			} else {
+				// Compare expected hostname with the DNS name
+				if (strcasecmp(hostname, dns_name) == 0) {
+					result = MatchFound;
+					break;
+				}
+			}
+		}
+	}
+	sk_GENERAL_NAME_pop_free(san_names, GENERAL_NAME_free);
+
+	return result;
+}
+
+
+validate_result
+validate_hostname(const char *hostname, const X509 *server_cert)
+{
+	validate_result result;
+
+	if ((hostname == NULL) || (server_cert == NULL)) {
+		return Error;
+	}
+
+	// First try the Subject Alternative Names extension
+	result = matches_subject_alternative_name(hostname, server_cert);
+	if (result == NoSANPresent) {
+		// Extension was not found: try the Common Name
+		result = matches_common_name(hostname, server_cert);
+	}
+
+	return result;
+}
+
 SSL *
 ssl_connect(int fd, char *hostname)
 {
-
+	X509 *server_cert;
 	SSL_CTX *ssl_ctx;
 	SSL *ssl;
 
 	ssl_startup();
 
 	ssl_ctx = SSL_CTX_new(SSLv23_client_method());
-	ssl = SSL_new(ssl_ctx);
 	if (!conf->insecure) {
-		SSL_set_hostflags(ssl, X509_CHECK_FLAG_NO_PARTIAL_WILDCARDS);
-		SSL_set1_host(ssl, hostname);
 		SSL_CTX_set_default_verify_paths(ssl_ctx);
 		SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER, NULL);
 	}
 	SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
 
+	ssl = SSL_new(ssl_ctx);
 	SSL_set_fd(ssl, fd);
 	SSL_set_tlsext_host_name(ssl, hostname);
 
@@ -99,14 +213,32 @@ ssl_connect(int fd, char *hostname)
 	if (err <= 0) {
 		fprintf(stderr, _("SSL error: %s\n"),
 			ERR_reason_error_string(ERR_get_error()));
+		SSL_CTX_free(ssl_ctx);
 		return NULL;
 	}
 
 	err = SSL_get_verify_result(ssl);
 	if (err != X509_V_OK) {
 		fprintf(stderr, _("SSL error: Certificate error"));
+		SSL_CTX_free(ssl_ctx);
 		return NULL;
 	}
+
+	server_cert =  SSL_get_peer_certificate(ssl);
+	if (server_cert == NULL) {
+		fprintf(stderr, _("SSL error: Certificate not found"));
+		SSL_CTX_free(ssl_ctx);
+		return NULL;
+	}
+
+	if (validate_hostname(hostname, server_cert) != MatchFound) {
+		fprintf(stderr, _("SSL error: Hostname verification failed"));
+		X509_free(server_cert);
+		SSL_CTX_free(ssl_ctx);
+		return NULL;
+	}
+
+	X509_free(server_cert);
 
 	return ssl;
 }
