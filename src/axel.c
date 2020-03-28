axel-download-accelerator/axel

commit 9653399f493a933033c389c2691951af5077c69f

lib: Add ASN1_STRING_get0_data & improve prototype handling
- compat-bsd.h contains common BSD libc function prototypes
- compat-ssl.h contains OpenSSL/LibreSSL function prototypes
- compat-android.h contains Android compatibility stuff

Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/axel.h
@@ -68,7 +68,9 @@
 #include <arpa/inet.h>
 #include <net/if.h>
 #include <pthread.h>
-#include "android-compat.h"
+#include "compat-android.h"
+#include "compat-bsd.h"
+#include "compat-ssl.h"
 
 /* Internationalization */
 #ifdef ENABLE_NLS
@@ -138,14 +140,5 @@ void axel_close(axel_t *axel);
 void print_messages(axel_t *axel);
 
 double axel_gettime(void);
-
-
-#ifndef HAVE_STRLCAT
-size_t strlcat(char *, const char *, size_t);
-#endif /* HAVE_strlcat */
-
-#ifndef HAVE_STRLCPY
-size_t strlcpy(char *, const char *, size_t);
-#endif /* HAVE_STRLCPY */
 
 #endif				/* AXEL_AXEL_H */

src/ssl.c
@@ -37,9 +37,8 @@
 
 /* SSL interface */
 
-#include "axel.h"
-
 #include <openssl/err.h>
+#include "axel.h"
 
 static pthread_mutex_t ssl_lock;
 static bool ssl_inited = false;

src/ssl_verify.c
@@ -1,6 +1,7 @@
 /*
   Helper functions to perform basic hostname validation using OpenSSL.
 
+  Copyright 2020 Ismael Luceno
   Author:  Alban Diquet
   Copyright (C) 2012, iSEC Partners.
 
@@ -23,16 +24,9 @@
   SOFTWARE.
  */
 
-#include "axel.h"
-
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
-
-#if OPENSSL_VERSION_NUMBER < 0x10101000L
-#define ASN1_STRING_data_compat ASN1_STRING_data
-#else
-#define ASN1_STRING_data_compat ASN1_STRING_get0_data
-#endif
+#include "axel.h"
 
 typedef enum {
 	MatchFound,
@@ -67,7 +61,7 @@ ssl_matches_common_name(const char *hostname, const X509 *server_cert)
 	if (common_name_asn1 == NULL) {
 		return Error;
 	}
-	common_name_str = (char *) ASN1_STRING_data_compat(common_name_asn1);
+	common_name_str = (char *) ASN1_STRING_get0_data(common_name_asn1);
 
 	// Make sure there isn't an embedded NUL character in the CN
 	if ((size_t) ASN1_STRING_length(common_name_asn1) != strlen(common_name_str)) {
@@ -103,7 +97,7 @@ ssl_matches_subject_alternative_name(const char *hostname, const X509 *server_ce
 
 		if (current_name->type == GEN_DNS) {
 			// Current name is a DNS name, let's check it
-			char *dns_name = (char *) ASN1_STRING_data_compat(current_name->d.dNSName);
+			char *dns_name = (char *) ASN1_STRING_get0_data(current_name->d.dNSName);
 
 			// Make sure there isn't an embedded NUL character in the DNS name
 			if ((size_t) ASN1_STRING_length(current_name->d.dNSName) != strlen(dns_name)) {

src/tcp.c
@@ -42,13 +42,12 @@
 
 #define _POSIX_C_SOURCE 200112L
 
-#include "axel.h"
-
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
 #include <sys/ioctl.h>
 #include <netinet/tcp.h>
+#include "axel.h"
 
 #ifndef TCP_FASTOPEN_CONNECT
 #ifdef __linux__

src/text.c
@@ -45,9 +45,8 @@
 
 /* Text interface */
 
-#include "axel.h"
-
 #include <sys/ioctl.h>
+#include "axel.h"
 
 
 static void stop(int signal);

compat-bsd.h
@@ -1,0 +1,7 @@
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *, const char *, size_t);
+#endif
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *, const char *, size_t);
+#endif

compat-ssl.h
@@ -1,0 +1,3 @@
+#if defined(HEADER_ASN1_H) && !defined(HAVE_ASN1_STRING_GET0_DATA)
+const unsigned char *ASN1_STRING_get0_data(const ASN1_STRING *);
+#endif

commit 6f9e558660b04963d41978e40a4eb899659c2b28

Move inclusion of config.h to the top of each file
A change in order of inclusion of axel.h broke build against Mac OS X.

Solution: In each file config.h must be included first and axel.h last.

Fixes: 9653399f493a ("lib: Add ASN1_STRING_get0_data & improve proto"...)
Fixes: https://github.com/axel-download-accelerator/axel/issues/275
Ref:   45d68a239b16 ("configure: Workaround API bug on Darwin / Mac OS X")
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/axel.c
@@ -46,6 +46,7 @@
 
 /* Main control */
 
+#include "config.h"
 #include "axel.h"
 #include "assert.h"
 #include "sleep.h"

src/axel.h
@@ -43,8 +43,6 @@
 #ifndef AXEL_AXEL_H
 #define AXEL_AXEL_H
 
-#include "config.h"
-
 #include <time.h>
 #include <ctype.h>
 #include <fcntl.h>

src/conf.c
@@ -41,6 +41,7 @@
 
 /* Configuration handling file */
 
+#include "config.h"
 #include "axel.h"
 
 /* Some nifty macro's.. */

src/conn.c
@@ -43,6 +43,7 @@
 
 /* Connection stuff */
 
+#include "config.h"
 #include "axel.h"
 
 /**

src/ftp.c
@@ -38,6 +38,7 @@
 
 /* FTP control file */
 
+#include "config.h"
 #include "axel.h"
 
 int

src/http.c
@@ -45,6 +45,7 @@
 
 /* HTTP control file */
 
+#include "config.h"
 #include "axel.h"
 
 inline static int

src/search.c
@@ -40,6 +40,7 @@
 
 /* filesearching.com searcher */
 
+#include "config.h"
 #include "axel.h"
 #include "sleep.h"
 

src/ssl.c
@@ -37,6 +37,7 @@
 
 /* SSL interface */
 
+#include "config.h"
 #include <openssl/err.h>
 #include "axel.h"
 

src/ssl_verify.c
@@ -24,6 +24,7 @@
   SOFTWARE.
  */
 
+#include "config.h"
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
 #include "axel.h"

src/tcp.c
@@ -42,6 +42,7 @@
 
 #define _POSIX_C_SOURCE 200112L
 
+#include "config.h"
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>

src/text.c
@@ -45,6 +45,7 @@
 
 /* Text interface */
 
+#include "config.h"
 #include <sys/ioctl.h>
 #include "axel.h"
 