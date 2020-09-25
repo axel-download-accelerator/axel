axel-download-accelerator/axel

commit ec63c397d8394a4eb64682f1c0497d6a0cc4859f

Fix usage of conn->lastbyte, off by one
Fixes: https://github.com/axel-download-accelerator/axel/issues/209
Fixes: 06fa69faf4fe9571e9c0ffd4a3c255fdb696f303
       ("Fix HTTP request-ranges to be zero-based")

Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/axel.c
@@ -362,12 +362,12 @@ reactivate_connection(axel_t *axel, int thread)
 	int idx = -1;
 
 	if (axel->conn[thread].enabled ||
-	    axel->conn[thread].currentbyte <= axel->conn[thread].lastbyte)
+	    axel->conn[thread].currentbyte < axel->conn[thread].lastbyte)
 		return;
 	/* find some more work to do */
 	for (int j = 0; j < axel->conf->num_connections; j++) {
 		long long int remaining =
-		    axel->conn[j].lastbyte - axel->conn[j].currentbyte + 1;
+		    axel->conn[j].lastbyte - axel->conn[j].currentbyte;
 		if (remaining > max_remaining) {
 			max_remaining = remaining;
 			idx = j;
@@ -381,7 +381,7 @@ reactivate_connection(axel_t *axel, int thread)
 		axel->conn[thread].lastbyte = axel->conn[idx].lastbyte;
 		axel->conn[idx].lastbyte =
 		    axel->conn[idx].currentbyte + max_remaining / 2;
-		axel->conn[thread].currentbyte = axel->conn[idx].lastbyte + 1;
+		axel->conn[thread].currentbyte = axel->conn[idx].lastbyte;
 	}
 }
 
@@ -408,13 +408,10 @@ axel_start(axel_t *axel)
 	if (axel->conf->verbose > 0)
 		axel_message(axel, _("Starting download"));
 
-	for (i = 0; i < axel->conf->num_connections; i++)
+	for (i = 0; i < axel->conf->num_connections; i++) {
 		if (axel->conn[i].currentbyte > axel->conn[i].lastbyte) {
 			reactivate_connection(axel, i);
-		}
-
-	for (i = 0; i < axel->conf->num_connections; i++)
-		if (axel->conn[i].currentbyte <= axel->conn[i].lastbyte) {
+		} else if (axel->conn[i].currentbyte < axel->conn[i].lastbyte) {
 			if (axel->conf->verbose >= 2) {
 				axel_message(axel,
 					     _("Connection %i downloading from %s:%i using interface %s"),
@@ -431,6 +428,7 @@ axel_start(axel_t *axel)
 				axel->ready = -1;
 			}
 		}
+	}
 
 	/* The real downloading will start now, so let's start counting */
 	axel->start_time = axel_gettime();
@@ -587,7 +585,7 @@ axel_do(axel_t *axel)
 			continue;
 
 		if (!axel->conn[i].enabled &&
-		    axel->conn[i].currentbyte <= axel->conn[i].lastbyte) {
+		    axel->conn[i].currentbyte < axel->conn[i].lastbyte) {
 			if (!axel->conn[i].state) {
 				// Wait for termination of this thread
 				pthread_join(*(axel->conn[i].setup_thread),
@@ -850,7 +848,7 @@ axel_divide(axel_t *axel)
 
 	for (int i = 0; i < axel->conf->num_connections; i++) {
 		axel->conn[i].currentbyte = seg_len * i;
-		axel->conn[i].lastbyte    = seg_len * i + seg_len - 1;
+		axel->conn[i].lastbyte    = seg_len * i + seg_len;
 	}
 
 	/* Last connection downloads remaining bytes */

commit 40863854fec9c241a25ee295b4f4b40b085cb295

http: Fix request range comparison and overflow
The issue wasn't completely corrected by commit
ec63c397d8394a4eb64682f1c0497d6a0cc4859f
("Fix usage of conn->lastbyte, off by one")

[ismael@iodev.co.uk: merged commits and edited message]
Closes: https://github.com/axel-download-accelerator/axel/issues/322
Fixes: ec63c397d8394a4eb64682f1c0497d6a0cc4859f
       ("Fix usage of conn->lastbyte, off by one")
Fixes: 06fa69faf4fe9571e9c0ffd4a3c255fdb696f303
       ("Fix HTTP request-ranges to be zero-based")

src/axel.c
@@ -453,7 +453,7 @@ axel_start(axel_t *axel)
 		axel_message(axel, _("Starting download"));
 
 	for (i = 0; i < axel->conf->num_connections; i++) {
-		if (axel->conn[i].currentbyte > axel->conn[i].lastbyte) {
+		if (axel->conn[i].currentbyte >= axel->conn[i].lastbyte) {
 			pthread_mutex_lock(&axel->conn[i].lock);
 			reactivate_connection(axel, i);
 			pthread_mutex_unlock(&axel->conn[i].lock);
@@ -571,7 +571,7 @@ axel_do(axel_t *axel)
 		if (size == 0) {
 			if (axel->conf->verbose) {
 				/* Only abnormal behaviour if: */
-				if (axel->conn[i].currentbyte <=
+				if (axel->conn[i].currentbyte <
 				    axel->conn[i].lastbyte &&
 				    axel->size != LLONG_MAX) {
 					axel_message(axel,

src/http.c
@@ -195,7 +195,7 @@ http_get(http_t *conn, char *lurl)
 	if (conn->firstbyte >= 0) {
 		if (conn->lastbyte)
 			http_addheader(conn, "Range: bytes=%lld-%lld",
-				       conn->firstbyte, conn->lastbyte);
+				       conn->firstbyte, conn->lastbyte - 1);
 		else
 			http_addheader(conn, "Range: bytes=%lld-",
 				       conn->firstbyte);