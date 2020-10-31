axel-download-accelerator/axel

commit e51c66cbced66ef55566d3ced586973bbb61e899

configure: Add AXEL_CHECK_MACRO
To detect the presence/absence of macros and enum values in system headers.

Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

acinclude.m4
@@ -68,3 +68,20 @@ m4_define([AC_PACKAGE_BUGREPORT], [$2])
 
 # MKINSTALLDIRS isn't needed; remove
 AC_DEFUN([AM_MKINSTALLDIRS], [])
+
+# AXEL_CHECK_MACRO(MACRO, HEADER, [ACTION-IF-FOUND], [ACTION-IF-NOT-FOUND])
+# ---------------------------------------------------------------------
+AC_DEFUN([AXEL_CHECK_MACRO],
+[_AC_INIT_LITERAL([$1])
+_AC_INIT_LITERAL([$2])
+AC_CACHE_CHECK([for $1], [axel_cv_macro_$1],
+  [AC_COMPILE_IFELSE(
+    [AC_LANG_SOURCE([[#include <]$2[>]], [[(void)]$1;])],
+      [axel_cv_macro_$1=yes],
+      [axel_cv_macro_$1=no])
+  ])
+AS_IF([test "x$axel_cv_macro_$1" = "xyes"],
+  [$3], m4_default([$4], [
+    AC_MSG_ERROR([The $1 macro is required; it should be defined by $2.])
+  ]))
+])

commit 46237ae5f5b0658d3de7bdec19619c18fbf0784a

configure: Use AC_LANG_PROGRAM in AXEL_CHECK_MACRO
Fixes: e51c66cbced6 ("configure: Add AXEL_CHECK_MACRO")
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

acinclude.m4
@@ -76,7 +76,7 @@ AC_DEFUN([AXEL_CHECK_MACRO],
 _AC_INIT_LITERAL([$2])
 AC_CACHE_CHECK([for $1], [axel_cv_macro_$1],
   [AC_COMPILE_IFELSE(
-    [AC_LANG_SOURCE([[#include <]$2[>]], [[(void)]$1;])],
+    [AC_LANG_PROGRAM([[#include <]$2[>]], [[(void)]$1;])],
       [axel_cv_macro_$1=yes],
       [axel_cv_macro_$1=no])
   ])