axel-download-accelerator/axel

commit 51f81442c09aeb5cbed439089fc32b9cda9fc853

http: Only send Range header if required
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/http.c
@@ -192,13 +192,12 @@ http_get(http_t *conn, char *lurl)
 		http_addheader(conn, "Proxy-Authorization: Basic %s",
 			       conn->proxy_auth);
 	http_addheader(conn, "Accept: */*");
-	if (conn->firstbyte >= 0) {
-		if (conn->lastbyte)
-			http_addheader(conn, "Range: bytes=%lld-%lld",
-				       conn->firstbyte, conn->lastbyte - 1);
-		else
-			http_addheader(conn, "Range: bytes=%lld-",
-				       conn->firstbyte);
+	if (conn->lastbyte && conn->firstbyte >= 0) {
+		http_addheader(conn, "Range: bytes=%lld-%lld",
+			       conn->firstbyte, conn->lastbyte - 1);
+	} else if (conn->firstbyte > 0) {
+		http_addheader(conn, "Range: bytes=%lld-",
+			       conn->firstbyte);
 	}
 }
 

commit 2fa16c4fc1883b7a9d563b76523c29474d4a8edf

http: Restore the Range header in requests
It's required to determine if there's support for ranged requests.

Fixes: 51f81442c09a ("http: Only send Range header if required")
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/http.c
@@ -191,7 +191,7 @@ http_get(http_t *conn, char *lurl)
 	if (conn->lastbyte && conn->firstbyte >= 0) {
 		http_addheader(conn, "Range: bytes=%lld-%lld",
 			       conn->firstbyte, conn->lastbyte - 1);
-	} else if (conn->firstbyte > 0) {
+	} else if (conn->firstbyte >= 0) {
 		http_addheader(conn, "Range: bytes=%lld-",
 			       conn->firstbyte);
 	}