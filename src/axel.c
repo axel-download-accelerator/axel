axel-download-accelerator/axel

commit 9cedf8be29dbb6ff8d814f821d62b9822b2536d6

axel: Fix speed limiting, and use 64-bit sizes and offsets
Reset delay_time only when it's too small for another subtraction,
the original code wrongly cleared it when the speed is within spec.

Variables used to overflow on 32-bit systems, use off_t instead, which
should be 64 bits on systems supporting that.

[ismael@iodev.co.uk: Improved commit message]
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/axel.c
@@ -230,7 +230,7 @@ axel_new(conf_t *conf, int count, const search_t *res)
 		if (axel->size != LLONG_MAX) {
 			char hsize[32];
 			axel_size_human(hsize, sizeof(hsize), axel->size);
-			axel_message(axel, _("File size: %s (%zu bytes)"),
+			axel_message(axel, _("File size: %s (%jd bytes)"),
 				     hsize, axel->size);
 		} else {
 			axel_message(axel, _("File size: unavailable"));
@@ -339,7 +339,7 @@ axel_open(axel_t *axel)
 		}
 
 		axel_message(axel,
-			     _("State file found: %zu bytes downloaded, %zu to go."),
+			     _("State file found: %jd bytes downloaded, %jd to go."),
 			     axel->bytes_done, axel->size - axel->bytes_done);
 
 		close(fd);
@@ -372,7 +372,7 @@ axel_open(axel_t *axel)
 				     _("Crappy filesystem/OS.. Working around. :-("));
 			lseek(axel->outfd, 0, SEEK_SET);
 			memset(buffer, 0, axel->conf->buffer_size);
-			long long int j = axel->size;
+			off_t j = axel->size;
 			while (j > 0) {
 				ssize_t nwrite;
 
@@ -404,7 +404,7 @@ void
 reactivate_connection(axel_t *axel, int thread)
 {
 	/* TODO Make the minimum also depend on the connection speed */
-	long long int max_remaining = MIN_CHUNK_WORTH - 1;
+	off_t max_remaining = MIN_CHUNK_WORTH - 1;
 	int idx = -1;
 
 	if (axel->conn[thread].enabled ||
@@ -412,8 +412,8 @@ reactivate_connection(axel_t *axel, int thread)
 		return;
 
 	for (int j = 0; j < axel->conf->num_connections; j++) {
-		long long int remaining =
-		    axel->conn[j].lastbyte - axel->conn[j].currentbyte;
+		off_t remaining =
+			axel->conn[j].lastbyte - axel->conn[j].currentbyte;
 		if (remaining > max_remaining) {
 			max_remaining = remaining;
 			idx = j;
@@ -489,11 +489,11 @@ axel_do(axel_t *axel)
 {
 	fd_set fds[1];
 	int hifd, i;
-	long long int remaining, size;
+	off_t remaining, size;
 	struct timeval timeval[1];
 	url_t *url_ptr;
 	struct timespec delay = {.tv_sec = 0, .tv_nsec = 100000000};
-	unsigned int max_speed_ratio;
+	unsigned long long int max_speed_ratio;
 
 	/* Create statefile if necessary */
 	if (axel_gettime() > axel->next_state) {
@@ -675,7 +675,7 @@ axel_do(axel_t *axel)
 
 	/* Calculate current average speed and finish_time */
 	axel->bytes_per_second =
-	    (int)((double)(axel->bytes_done - axel->start_byte) /
+	    (off_t)((double)(axel->bytes_done - axel->start_byte) /
 		  (axel_gettime() - axel->start_time));
 	if (axel->bytes_per_second != 0)
 		axel->finish_time =
@@ -698,10 +698,10 @@ axel_do(axel_t *axel)
 			} else if (axel->delay_time.tv_sec > 0) {
 				axel->delay_time.tv_sec--;
 				axel->delay_time.tv_nsec += 999000000;
+			} else {
+				axel->delay_time.tv_sec = 0;
+				axel->delay_time.tv_nsec = 0;
 			}
-		} else {
-			axel->delay_time.tv_sec = 0;
-			axel->delay_time.tv_nsec = 0;
 		}
 		if (axel_sleep(axel->delay_time) < 0) {
 			axel_message(axel,
@@ -882,12 +882,12 @@ static void
 axel_divide(axel_t *axel)
 {
 	/* Optimize the number of connections in case the file is small */
-	size_t maxconns = max(1u, axel->size / MIN_CHUNK_WORTH);
+	off_t maxconns = max(1u, axel->size / MIN_CHUNK_WORTH);
 	if (maxconns < axel->conf->num_connections)
 		axel->conf->num_connections = maxconns;
 
 	/* Calculate each segment's size */
-	size_t seg_len = axel->size / axel->conf->num_connections;
+	off_t seg_len = axel->size / axel->conf->num_connections;
 
 	if (!seg_len) {
 		printf(_("Too few bytes remaining, forcing a single connection\n"));

src/axel.h
@@ -129,8 +129,8 @@ typedef struct {
 	char filename[MAX_STRING];
 	double start_time;
 	int next_state, finish_time;
-	size_t bytes_done, start_byte, size;
-	int bytes_per_second;
+	off_t bytes_done, start_byte, size;
+	long long int bytes_per_second;
 	struct timespec delay_time;
 	int outfd;
 	int ready;

src/conf.c
@@ -161,10 +161,21 @@ conf_loadfile(conf_t *conf, const char *file)
 			KEY(search_amount)
 			KEY(search_top)
 		else
-			goto other_keys;
+			goto long_num_keys;
 
 		/* Save numeric option */
 		*((int *)dst) = atoi(value);
+		continue;
+
+		/* Long numeric options */
+ long_num_keys:
+		MATCH
+			KEY(max_speed)
+		else
+			goto other_keys;
+
+		/* Save numeric option */
+		*((unsigned long long *)dst) = strtoull(value, NULL, 10);
 		continue;
 
  other_keys:

src/conf.h
@@ -53,7 +53,7 @@ typedef struct {
 	int reconnect_delay;
 	int max_redirect;
 	int buffer_size;
-	int max_speed;
+	unsigned long long max_speed;
 	int verbose;
 	int alternate_output;
 	int insecure;

src/conn.c
@@ -302,7 +302,7 @@ conn_setup(conn_t *conn)
 		conn->tcp = &conn->ftp->data_tcp;
 
 		if (conn->currentbyte) {
-			ftp_command(conn->ftp, "REST %lld", conn->currentbyte);
+			ftp_command(conn->ftp, "REST %jd", conn->currentbyte);
 			if (ftp_wait(conn->ftp) / 100 != 3 &&
 			    conn->ftp->status / 100 != 2)
 				return 0;

src/conn.h
@@ -85,9 +85,9 @@ typedef struct {
 
 	ftp_t ftp[1];
 	http_t http[1];
-	long long int size;	/* File size, not 'connection size'.. */
-	long long int currentbyte;
-	long long int lastbyte;
+	off_t size; /* File size, not 'connection size'.. */
+	off_t currentbyte;
+	off_t lastbyte;
 	tcp_t *tcp;
 	bool enabled;
 	bool supported;

src/ftp.c
@@ -111,17 +111,17 @@ ftp_cwd(ftp_t *conn, char *cwd)
 }
 
 /* Get file size. Should work with all reasonable servers now */
-long long int
+off_t
 ftp_size(ftp_t *conn, char *file, int maxredir, unsigned io_timeout)
 {
-	long long int i, j, size = MAX_STRING;
+	off_t i, j, size = MAX_STRING;
 	char *reply, *s, fn[MAX_STRING];
 
 	/* Try the SIZE command first, if possible */
 	if (!strchr(file, '*') && !strchr(file, '?')) {
 		ftp_command(conn, "SIZE %s", file);
 		if (ftp_wait(conn) / 100 == 2) {
-			sscanf(conn->message, "%*i %lld", &i);
+			sscanf(conn->message, "%*i %jd", &i);
 			return i;
 		} else if (conn->status / 10 != 50) {
 			fprintf(stderr, _("File not found.\n"));
@@ -213,10 +213,10 @@ ftp_size(ftp_t *conn, char *file, int maxredir, unsigned io_timeout)
 	   possible wildcards. */
 	else {
 		s = strstr(reply, "\n-");
-		i = sscanf(s, "%*s %*i %*s %*s %lld %*s %*i %*s %100s", &size,
+		i = sscanf(s, "%*s %*i %*s %*s %jd %*s %*i %*s %100s", &size,
 			   fn);
 		if (i < 2) {
-			i = sscanf(s, "%*s %*i %lld %*i %*s %*i %*i %100s",
+			i = sscanf(s, "%*s %*i %jd %*i %*s %*i %*i %100s",
 				   &size, fn);
 			if (i < 2) {
 				return -2;

src/ftp.h
@@ -65,6 +65,6 @@ __attribute__((format(printf, 2, 3)))
 int ftp_command(ftp_t *conn, const char *format, ...);
 int ftp_cwd(ftp_t *conn, char *cwd);
 int ftp_data(ftp_t *conn, unsigned io_timeout);
-long long int ftp_size(ftp_t *conn, char *file, int maxredir, unsigned io_timeout);
+off_t ftp_size(ftp_t *conn, char *file, int maxredir, unsigned io_timeout);
 
 #endif				/* AXEL_FTP_H */

commit a80db5b1843cc81b242c4e5feea06b49c405d71e

axel: Fix debug message for off_t changes
Fixes: 9cedf8be29db ("axel: Fix speed limiting, and use 64-bit sizes and
offsets")

Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/axel.c
@@ -913,7 +913,7 @@ axel_divide(axel_t *axel)
 	axel->conn[axel->conf->num_connections - 1].lastbyte += tail;
 #ifndef NDEBUG
 	for (int i = 0; i < axel->conf->num_connections; i++) {
-		printf(_("Downloading %lld-%lld using conn. %i\n"),
+		printf(_("Downloading %jd-%jd using conn. %i\n"),
 		       axel->conn[i].currentbyte,
 		       axel->conn[i].lastbyte, i);
 	}