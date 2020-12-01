axel-download-accelerator/axel

commit 454d38772f8524b244f6986635ada03df5e1e038

ftp: ftp_wait: Optimize message termination
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/ftp.c
@@ -358,12 +358,8 @@ ftp_wait(ftp_t *conn)
 	fprintf(stderr, "fd(%i)-->%s", conn->tcp.fd, conn->message);
 #endif
 
-	if ((s = strchr(conn->message, '\n')) != NULL)
-		*s = 0;
-	if ((s = strchr(conn->message, '\r')) != NULL)
-		*s = 0;
-	conn->message =
-	    realloc(conn->message, max(strlen(conn->message) + 1, MAX_STRING));
-
+	int j = strcspn(conn->message, "\r\n");
+	conn->message[j] = 0;
+	conn->message = realloc(conn->message, j + 1);
 	return conn->status;
 }

commit c059c5429de9e54265582f85ffe3b08289f28d66

ftp: Fix duplicate variable j in ftp_wait
Fixes: 454d38772f85 ("ftp: ftp_wait: Optimize message termination")

src/ftp.c
@@ -358,8 +358,8 @@ ftp_wait(ftp_t *conn)
 	fprintf(stderr, "fd(%i)-->%s", conn->tcp.fd, conn->message);
 #endif
 
-	int j = strcspn(conn->message, "\r\n");
-	conn->message[j] = 0;
-	conn->message = realloc(conn->message, j + 1);
+	int k = strcspn(conn->message, "\r\n");
+	conn->message[k] = 0;
+	conn->message = realloc(conn->message, k + 1);
 	return conn->status;
 }