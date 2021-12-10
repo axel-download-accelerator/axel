axel-download-accelerator/axel

commit 454d38772f8524b244f6986635ada03df5e1e038

ftp: ftp_wait: Optimize message termination
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/ftp.c
@@ -358,12 +358,8 @@ ftp_wait(ftp_t *conn)
 	fprintf(stderr, "fd(%i)-->%s", conn->tcp.fd, conn->message);
 #endif
 
-	if ((s = strchr(conn->message, '\n')) != NULL)
-		*s = 0;
-	if ((s = strchr(conn->message, '\r')) != NULL)
-		*s = 0;
-	conn->message =
-	    realloc(conn->message, max(strlen(conn->message) + 1, MAX_STRING));
-
+	int j = strcspn(conn->message, "\r\n");
+	conn->message[j] = 0;
+	conn->message = realloc(conn->message, j + 1);
 	return conn->status;
 }

commit 14caa15b2fb6f0b5743e95ad0116f18eba6e13cc

Remove unused variable
Fixes: 454d38772f85 ("ftp: ftp_wait: Optimize message termination")
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/ftp.c
@@ -306,7 +306,6 @@ int
 ftp_wait(ftp_t *conn)
 {
 	int size = MAX_STRING, r = 0, complete, i, j;
-	char *s;
 
 	{
 		void *new_msg = realloc(conn->message, size);