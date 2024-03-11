axel-download-accelerator/axel

commit cad169bbc918a75596aec79384733861c77da156

random: Initialize fd at startup
Closes: https://github.com/axel-download-accelerator/axel/pull/425
Fixes: https://github.com/axel-download-accelerator/axel/issues/397
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/random.c
@@ -3,15 +3,20 @@
 #include "config.h"
 #include "axel.h"
 
+static int rnd_fd = -1;
+
+int
+axel_rnd_init(void)
+{
+	const char rnd_dev[] = "/dev/random";
+	rnd_fd = open(rnd_dev, O_RDONLY);
+	if (rnd_fd == -1)
+		perror(rnd_dev);
+	return rnd_fd;
+}
+
 ssize_t
 axel_rand64(uint64_t *out)
 {
-	static int fd = -1;
-	if (fd == -1) {
-		int tmp = open("/dev/random", O_RDONLY);
-		int expect = -1;
-		if (!atomic_compare_exchange_strong(&fd, &expect, tmp))
-			close(tmp);
-	}
-	return read(fd, out, sizeof(*out));
+	return read(rnd_fd, out, sizeof(*out));
 }

src/axel.h
@@ -149,6 +149,7 @@ void print_messages(axel_t *axel);
 
 double axel_gettime(void);
 ssize_t axel_rand64(uint64_t *);
+int axel_rnd_init(void);
 
 #define DN_MATCH_MALFORMED -1
 int dn_match(const char *hostname, const char *pat, size_t pat_len);

src/text.c
@@ -123,6 +123,8 @@ main(int argc, char *argv[])
 	bindtextdomain(PACKAGE, LOCALEDIR);
 	textdomain(PACKAGE);
 #endif
+	if (axel_rnd_init() == -1)
+		return 1;
 
 	if (!conf_init(conf)) {
 		return 1;

commit 65c3eff25931056f807bafffdc9473204d12c364

Remove inclusion of <stdatomic.h>
Fixes: cad169bbc918 ("random: Initialize fd at startup", 2024-02-05)
Signed-off-by: Ismael Luceno <ismael@iodev.co.uk>

src/random.c
@@ -1,4 +1,3 @@
-#include <stdatomic.h>
 #include <unistd.h>
 #include "config.h"
 #include "axel.h"